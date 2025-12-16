b32ee7440fe8063a12123a4441efc776afdc398c
mohneeshdaksh <mohneeshdaksh@outlook.com>
2025-12-15 17:19:12 -0800

    final version of the solution

--- Easy/Arrays/Plus One/plus_one.py
+++ Easy/Arrays/Plus One/plus_one.py
@@ -1,12 +1,20 @@
 # https://leetcode.com/problems/plus-one/description/
 
-digits = [1,2,3]
+digits = [9,9,9]
 
-for n in range(len(digits) - 1, -1, -1):
-    if digits[n] != 9:
-        digits[n] += 1
-        break
-    else:
+if all(d == 9 for d in digits) == True:
+    for n in range(len(digits)):
         digits[n] = 0
+    digits.insert(0, 1)
+
+else:
+    for n in range(len(digits) - 1, -1, -1):
+
+        if digits[n] == 9:
+            digits[n] = 0
+
+        else:
+            digits[n] += 1
+            break
 
 print(digits)
