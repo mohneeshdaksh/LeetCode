d6a9c8233cf08937ec7304884ad350f836513261
mohneeshdaksh <mohneeshdaksh@outlook.com>
2025-12-19 12:42:56 -0800

    Bug fix

--- Easy/Arrays/Majority Element/majority_element.py
+++ Easy/Arrays/Majority Element/majority_element.py
@@ -1,10 +1,10 @@
 # https://leetcode.com/problems/majority-element/description/
 
-nums = [2,2,1,1,1,2,2]
+# nums = [2,2,1,1,2,1,2]
 
 # nums = [1]
 
-# nums = [3,1,3,2,3,2,3]
+nums = [3,2,3,2,3,3,2]
 
 # nums = [2,3,3,2,2]
 
@@ -14,10 +14,14 @@ max_count = 1
 for current_element in nums[1:]:
     if current_element == majority_element:
         max_count += 1
+    elif max_count == 0:
+        majority_element = current_element
+        max_count = 1
     else:
         max_count -= 1
-        if max_count == 0:
-            majority_element = current_element
-            max_count += 1
 
-print(majority_element)
+if max_count == 0:
+    print("No majority element")
+else:
+    print(majority_element)
+
