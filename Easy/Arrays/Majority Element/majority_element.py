5399c35eb58354767f1ed91ddeb2be38038957d5
mohneeshdaksh <mohneeshdaksh@outlook.com>
2025-12-18 16:28:46 -0800

    code logic enhancement

--- Easy/Arrays/Majority Element/majority_element.py
+++ Easy/Arrays/Majority Element/majority_element.py
@@ -2,3 +2,22 @@
 
 nums = [2,2,1,1,1,2,2]
 
+# nums = [1]
+
+# nums = [3,1,3,2,3,2,3]
+
+# nums = [2,3,3,2,2]
+
+majority_element = nums[0]
+max_count = 1
+
+for current_element in nums[1:]:
+    if current_element == majority_element:
+        max_count += 1
+    else:
+        max_count -= 1
+        if max_count == 0:
+            majority_element = current_element
+            max_count += 1
+
+print(majority_element)
