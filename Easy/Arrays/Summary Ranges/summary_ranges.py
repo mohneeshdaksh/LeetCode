908a3614413d1752f406d02a01ec9af87fd475bd
mohneeshdaksh <mohneeshdaksh@outlook.com>
2025-12-20 00:05:11 -0800

    bug fix

--- Easy/Arrays/Summary Ranges/summary_ranges.py
+++ Easy/Arrays/Summary Ranges/summary_ranges.py
@@ -1,23 +1,22 @@
 # https://leetcode.com/problems/summary-ranges/description/
 
-nums = [0,2]
+nums = []
 
 summary_ranges = []
 
-start_range = 0
+if nums:
+    start_range = 0
 
-for current_index in range(1, len(nums)):
-    if nums[current_index] != nums[current_index - 1] + 1:
-        if current_index - 1 == start_range:
-            summary_ranges.append(f"{nums[current_index - 1]}")
-        else:
-            summary_ranges.append(f"{nums[start_range]}->{nums[current_index - 1]}")
-        start_range = current_index
-if current_index == len(nums)-1:
-    if current_index == start_range:
-        summary_ranges.append(f"{nums[current_index]}")
+    for current_index in range(1, len(nums)):
+        if nums[current_index] != nums[current_index - 1] + 1:
+            if current_index - 1 == start_range:
+                summary_ranges.append(f"{nums[current_index - 1]}")
+            else:
+                summary_ranges.append(f"{nums[start_range]}->{nums[current_index - 1]}")
+            start_range = current_index
+    if start_range == len(nums)-1:
+        summary_ranges.append(f"{nums[-1]}")
     else:
-        summary_ranges.append(f"{nums[start_range]}->{nums[current_index]}")
+        summary_ranges.append(f"{nums[start_range]}->{nums[-1]}")
 
 print(summary_ranges)
-        
