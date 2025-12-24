a761743f0f4c7a9fdbdee28fb51bfe64423db7b2
mohneeshdaksh <mohneeshdaksh@outlook.com>
2025-12-24 13:03:25 -0800

    modify solution

---
+++ Easy/String/Roman to Integer/roman_to_integer.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/roman-to-integer/description/
+
+s = "MCMXCIV"
+
+roman = {
+    "M":1000,
+    "D":500,
+    "C":100,
+    "L":50,
+    "X":10,
+    "V":5,
+    "I":1
+}
+
+i = 0
+integer = 0
+
+while i < len(s):
+    if i != (len(s) - 1) and roman[s[i]] < roman[s[i+1]]:
+        integer += (roman[s[i+1]] - roman[s[i]])
+        i = i+2
+    else:
+        integer += roman[s[i]]
+        i += 1
+print(integer)
